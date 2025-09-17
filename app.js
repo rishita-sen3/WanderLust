a938710b0d87caf4734e2a47d043241695c7193e
Rishita-sen3 <Rishitasen561@gmail.com>
2025-09-17 09:09:05 +0530

    Day7

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
 
 const app = express();
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -26,10 +27,22 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+
 // Root route
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
+
+const validateListing = (req, res, next) => {
+  const { error } = listingSchema.validate(req.body);
+  if(error) {
+    let errMsg = error.details.map(el => el.message).join(", ");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
 
 // Index Route
 app.get("/listings", wrapAsync(async (req, res) => {
@@ -59,14 +72,13 @@ app.get("/listings/:id", wrapAsync(async (req, res) => {
 }));
 
 // Create Route
-app.post("/listings", wrapAsync(async (req, res, next) => {
-    if(!req.body.listing) {
-      throw new ExpressError(400,"Send valid data for listing");
-    }
-    const newListing = new Listing(req.body.listing);
+app.post("/listings",validateListing,
+   wrapAsync(async (req, res, next) => {
+    const newListing = new Listing(req.body.listing); // Corrected here
     await newListing.save();
     res.redirect("/listings");
 }));
+
 
 // Edit Route
 app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
@@ -76,7 +88,9 @@ app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
 }));
 
 // Update Route 
-app.put("/listings/:id", wrapAsync(async (req, res) => {
+app.put("/listings/:id",
+  validateListing,
+  wrapAsync(async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect(`/listings/${id}`);
